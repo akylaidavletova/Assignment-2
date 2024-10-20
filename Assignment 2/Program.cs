ead868ea4daf3df5b7406583de1ffe144d6cb41d
Akylai Davletova <akylai.davletova17@gmail.com>
2024-10-19 22:59:45 -0400

    Add project files.

---
+++ Assignment 2/Program.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //I added extra words in titles during screenshots (such as what I am inputting) to make comparison easier
+            //I then deleted them during submission of the code to repo since we were asked not to modify much
+
+            // Question 1: Find Missing Numbers in Array
+            Console.WriteLine("Question 1:");
+            int[] nums1 = { 4, 3, 2, 7, 8, 2, 3, 1 };
+            IList<int> missingNumbers = FindMissingNumbers(nums1);
+            Console.WriteLine(string.Join(",", missingNumbers));
+
+            // Question 2: Sort Array by Parity
+            Console.WriteLine("Question 2:");
+            int[] nums2 = { 3, 1, 2, 4 };
+            int[] sortedArray = SortArrayByParity(nums2);
+            Console.WriteLine(string.Join(",", sortedArray));
+
+            // Question 3: Two Sum
+            Console.WriteLine("Question 3:");
+            int[] nums3 = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] indices = TwoSum(nums3, target);
+            Console.WriteLine(string.Join(",", indices));
+
+            // Question 4: Find Maximum Product of Three Numbers
+            Console.WriteLine("Question 4:");
+            int[] nums4 = { 1, 2, 3 };
+            int maxProduct = MaximumProduct(nums4);
+            Console.WriteLine(maxProduct);
+
+            // Question 5: Decimal to Binary Conversion
+            Console.WriteLine("Question 5:");
+            int decimalNumber = 42;
+            string binary = DecimalToBinary(decimalNumber);
+            Console.WriteLine(binary);
+
+            // Question 6: Find Minimum in Rotated Sorted Array
+            Console.WriteLine("Question 6:");
+            int[] nums5 = { 3, 4, 5, 1, 2 };
+            int minElement = FindMin(nums5);
+            Console.WriteLine(minElement);
+
+            // Question 7: Palindrome Number
+            Console.WriteLine("Question 7:");
+            int palindromeNumber = 121;
+            bool isPalindrome = IsPalindrome(palindromeNumber);
+            Console.WriteLine(isPalindrome);
+
+            // Question 8: Fibonacci Number
+            Console.WriteLine("Question 8:");
+            int n = 4;
+            int fibonacciNumber = Fibonacci(n);
+            Console.WriteLine(fibonacciNumber);
+        }
+
+        // Question 1: Find Missing Numbers in Array
+        public static IList<int> FindMissingNumbers(int[] nums)
+        {
+            try // I added my code below. I will solve only for first example of each question, such as: input [4, 3, 2, 7, 8, 2, 3, 1] and get Output of [5, 6] since we can't change much on top
+                // I removed placeholders as well for all questions where it had "return new int[0];"
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int index = Math.Abs(nums[i]) - 1;
+                    if (nums[index] > 0)
+                    {
+                        nums[index] = -nums[index];
+                    }
+                }
+                IList<int> missingNumbers = new List<int>();
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] > 0)
+                    {
+                        missingNumbers.Add(i + 1);
+                    }
+                }
+
+                return missingNumbers;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 2: Sort Array by Parity
+        public static int[] SortArrayByParity(int[] nums)
+        {
+            try
+            {
+                return nums //the shortest way to do these is to use linq
+                    .Where(num => num % 2 == 0).OrderBy(num => num)   //even numbers first      
+                    .Concat(nums.Where(num => num % 2 != 0).OrderBy(num => num)) // do the same for odd numbers
+                    .ToArray();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 3: Two Sum
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            try
+            {
+                Dictionary<int, int> numToIndex = new Dictionary<int, int>(); // store numbers and indexes in dictionary first
+                for (int i = 0; i < nums.Length; i++) // for loop to return indices of 2 numbers that add up to target
+                {
+                    int complement = target - nums[i];
+
+                    if (numToIndex.TryGetValue(complement, out int index))
+                    {
+                        return new int[] { index, i };
+                    }
+                    numToIndex[nums[i]] = i;
+                }
+                return new int[0]; // Placeholder
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 4: Find Maximum Product of Three Numbers
+        public static int MaximumProduct(int[] nums)
+        {
+            try
+            {
+                Array.Sort(nums);
+                int n = nums.Length;
+                return Math.Max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]); //question asks for max product of THREE #,so i did it manually. i know there are better ways to do it
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 5: Decimal to Binary Conversion
+        public static string DecimalToBinary(int decimalNumber)
+        {
+            try
+            {
+                return Convert.ToString(decimalNumber, 2); //convert to binary
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 6: Find Minimum in Rotated Sorted Array
+        public static int FindMin(int[] nums)
+        {
+            try
+            {
+                int left = 0, right = nums.Length - 1;
+                while (left < right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (nums[mid] > nums[right])
+                    {
+                        left = mid + 1;
+                    }
+                    else
+                    {
+                        right = mid;
+                    }
+                }
+                return nums[left]; // this tries to find the minimum element in a rotated sorted array
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 7: Palindrome Number
+        public static bool IsPalindrome(int x)
+        {
+            try
+            {
+                if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+                int reversed = 0;
+                while (x > reversed)
+                {
+                    reversed = reversed * 10 + x % 10;
+                    x /= 10;
+                }
+                return x == reversed || x == reversed / 10;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Question 8: Fibonacci Number
+        public static int Fibonacci(int n)
+        {
+            try
+            {
+                if (n <= 1) return n;
+                int a = 0, b = 1;
+                for (int i = 2; i <= n; i++)
+                {
+                    int temp = a + b;
+                    a = b;
+                    b = temp;
+                }
+                return b;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
